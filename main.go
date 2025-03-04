8f4876685cdb7e4a1a16be7ea71d20f3c62a5768
waik1ki <arbutus@outlook.kr>
2025-03-04 22:02:30 +0900

    feat: main 함수 서버 실행 로직 추가

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"gin-boiler-plate/config"
+	"gin-boiler-plate/database"
+	"gin-boiler-plate/handler"
+	"gin-boiler-plate/repository"
+	"gin-boiler-plate/routes"
+	"gin-boiler-plate/service"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := config.NewConfig()
+	db, err := database.NewDatabase(cfg.Driver, cfg.DSN, true)
+	if err != nil {
+		log.Fatal("Failed to connect database:", err)
+	}
+
+	r := repository.NewRepositories(db)
+	s := service.NewServices(r)
+	h := handler.NewHandlers(s)
+
+	e := gin.Default()
+	routes.NewRoutes(e, h)
+	e.Run(":3000")
+}

f3c88cdfade3c9bd68b98d1b1636e8bd225461e1
waik1ki <arbutus@outlook.kr>
2025-03-04 21:51:17 +0900

    feat: NewDatabase 함수 추가

--- database/database.go
+++ database/database.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"fmt"
+	"gin-boiler-plate/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDatabase(driver, dsn string, autoMigrate bool) (*gorm.DB, error) {
+	var db *gorm.DB
+	var err error
+
+	switch driver {
+	case "postgres":
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	default:
+		return nil, fmt.Errorf("unsupported database driver: %s", driver)
+	}
+
+	if autoMigrate {
+		if err := db.AutoMigrate(model.NewModels()...); err != nil {
+			return nil, fmt.Errorf("failed to auto migrate: %s", err)
+		}
+	}
+
+	return db, err
+}

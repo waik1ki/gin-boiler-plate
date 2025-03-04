dfa9401ef4b952be7400b8a3afe0e26cdf8cf45d
waik1ki <arbutus@outlook.kr>
2025-03-04 21:50:53 +0900

    feat: NewConfig 함수 추가

--- config/config.go
+++ config/config.go
@@ -1,1 +1,16 @@
 package config
+
+type Config struct {
+	Driver string
+	DSN    string
+}
+
+func NewConfig() *Config {
+	driver := "postgres"
+	dsn := "host=localhost user=postgres password=1234 dbname=testdb port=5432 sslmode=disable"
+
+	return &Config{
+		Driver: driver,
+		DSN:    dsn,
+	}
+}

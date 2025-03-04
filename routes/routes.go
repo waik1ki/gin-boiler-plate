3f0d34e67793e284e8ab89b79baa5e8ce497dd6a
waik1ki <arbutus@outlook.kr>
2025-03-04 22:02:07 +0900

    feat: Routes 생성 로직 추가

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"gin-boiler-plate/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRoutes(e *gin.Engine, handler *handler.Handlers) {
+	newUserRoutes(e.Group("/users"), handler.User)
+}
+
+func newUserRoutes(g *gin.RouterGroup, handler *handler.UserHandler) {
+	g.POST("", handler.CreateUser)
+	g.GET("", handler.GetUsers)
+	g.GET("/:id", handler.GetUser)
+	g.PUT("/:id", handler.UpdateUser)
+	g.DELETE("/:id", handler.DeleteUser)
+}

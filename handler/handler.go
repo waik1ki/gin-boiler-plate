02f4b1cedc204ec6ef9efd08d4e4523f87a10295
waik1ki <arbutus@outlook.kr>
2025-03-04 21:59:21 +0900

    feat: NewHandlers 함수 추가

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import "gin-boiler-plate/service"
+
+type Handlers struct {
+	User *UserHandler
+}
+
+func NewHandlers(services *service.Services) *Handlers {
+	return &Handlers{
+		User: newUserHandler(services.User),
+	}
+}

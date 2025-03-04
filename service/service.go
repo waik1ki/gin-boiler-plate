38db6648c4b8cddece58bd4d5acda32bd29d36ca
waik1ki <arbutus@outlook.kr>
2025-03-04 21:56:33 +0900

    feat: NewServices 함수 추가

--- service/service.go
+++ service/service.go
@@ -1,1 +1,13 @@
 package service
+
+import "gin-boiler-plate/repository"
+
+type Services struct {
+	User UserService
+}
+
+func NewServices(repos *repository.Repositories) *Services {
+	return &Services{
+		User: newUserService(repos.User),
+	}
+}

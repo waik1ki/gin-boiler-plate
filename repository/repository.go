be1981a6d7b15a2af530229521b1c9c4a8c25b78
waik1ki <arbutus@outlook.kr>
2025-03-04 21:55:45 +0900

    feat: NewRepositories 함수 추가

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,13 @@
 package repository
+
+import "gorm.io/gorm"
+
+type Repositories struct {
+	User UserRepository
+}
+
+func NewRepositories(db *gorm.DB) *Repositories {
+	return &Repositories{
+		User: newUserRepository(db),
+	}
+}
